d801f25261ab006ff10caf10d9344f4fdb690775
stevenrick <srick@eng.ucsd.edu>
2020-05-10 16:38:44 -0700

    webcam fun and haar cascade for face detection

--- webcam_and_opencv.py
+++ webcam_and_opencv.py
@@ -2,13 +2,21 @@ import cv2
 
 cap = cv2.VideoCapture(0)  # get image from webcam
 
+# mog2_bgs = cv2.createBackgroundSubtractorMOG2()
+# knn_bgs = cv2.createBackgroundSubtractorKNN()
+
 while True:
     ret, frame = cap.read()
     cv2.imshow('Original', frame)
 
-    edges = cv2.Canny(frame, 50, 100)
+    # mog2_mask = mog2_bgs.apply(frame)
+    # knn_mask = knn_bgs.apply(frame)
 
-    cv2.imshow('Edges', edges)
+    edges = cv2.Canny(frame, 50, 100)
+    cv2.imshow('edges', edges)
+
+    # cv2.imshow('MOG2', mog2_mask)
+    # cv2.imshow('KNN', knn_mask)
 
     k = cv2.waitKey(5) & 0xFF  # press ESC to quit
     if k == 27:
